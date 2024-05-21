80987d8b257c7383e21f76e75b66991020c2fede
Thais Alibert <talibert@z1r8p2.42lyon.fr>
2024-05-21 19:06:16 +0200

    merge

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -27,29 +26,3 @@ typedef struct s_commands
 }				t_commands;
 
 #endif
-=======
-#ifndef		MINISHELL_H
-# define	MINISHELL_H
-# include <stdbool.h>
-
-typedef struct s_file
-{
-	char	*name;
-	int		fd;
-	bool	state;
-}				t_file;
-
-typedef struct s_commands
-{
-	char				*name;
-	char 				**paths;
-	char				**flags;
-	int					index;
-	void				(*f)(void*);
-	t_file				input;
-	t_file				output;
-	struct s_commands	*next;
-}				t_commands;
-
-#endif
->>>>>>> 3c15b3ef40cf3445a6d56f4fcb72b3cad46852c8
