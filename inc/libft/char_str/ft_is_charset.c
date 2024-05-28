c8972fea4b2444cdddc4e74fac20156cdc9fc348
Adri-cncl <cancel.adrien@gmail.com>
2024-05-28 19:57:10 +0200

    readline title with allowed functions

--- inc/libft/char_str/ft_is_charset.c
+++ inc/libft/char_str/ft_is_charset.c
@@ -6,20 +6,20 @@
 /*   By: acancel <acancel@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 16:58:02 by acancel           #+#    #+#             */
-/*   Updated: 2023/11/27 15:04:04 by acancel          ###   ########lyon.fr   */
+/*   Updated: 2024/05/28 19:35:32 by acancel          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_is_charset(char s1, char const *set)
+int	ft_is_charset(char c, char const *set)
 {
 	size_t	i;
 
 	i = 0;
 	while (set[i])
 	{
-		if (set[i] == s1)
+		if (set[i] == c)
 			return (1);
 		i++;
 	}
