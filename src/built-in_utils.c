6a2028047dbcb55bf324ddc28a3b5d856447bbb0
Adri-cncl <cancel.adrien@gmail.com>
2024-06-30 16:20:46 +0200

    WIP and normed ft_is_built-in

--- src/built-in_utils.c
+++ src/built-in_utils.c
@@ -1,21 +1,27 @@
 #include "minishell.h"
 
+static void	ft_witch_cd(t_data *data)
+{
+	if (data->cmds->args[1] == NULL || \
+		ft_strncmp(data->cmds->args[1], "~", 1) == 0)
+	{
+		ft_cd(data->home, data);
+		ft_echo(data);
+		free_data(data);
+	}
+	else
+	{
+		ft_cd(ft_substr(data->line, 3, ft_strlen(data->line) - 3), data);
+		ft_echo(data);
+		free_data(data);
+	}
+}
+
 int	ft_is_built_in(t_data *data)
 {
 	if (ft_strncmp(data->cmds->name, "cd", 3) == 0)
 	{
-		if (data->cmds->args[1] == NULL || ft_strncmp(data->cmds->args[1], "~", 1) == 0)
-		{
-			ft_cd(data->home, data);
-			ft_echo(data);
-			free_data(data);
-		}
-		else
-		{
-			ft_cd(ft_substr(data->line, 3, ft_strlen(data->line) - 3), data);
-			ft_echo(data);
-			free_data(data);
-		}
+		ft_witch_cd(data);
 		return (1);
 	}
 	if (ft_strncmp(data->cmds->name, "echo", 5) == 0)
