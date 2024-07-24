63b49909687479e9fad03ead3becb1aa1a53d603
Adri-cncl <cancel.adrien@gmail.com>
2024-07-24 02:27:08 +0200

    Normed again & again

--- src/export.c
+++ src/export.c
@@ -1,13 +1,14 @@
 #include "minishell.h"
 
-int ft_envcmp(char *env, char *variable)
+int	ft_envcmp(char *env, char *variable)
 {
 	char	*temp;
 	int		res;
 
 	res = 1;
 	temp = ft_strjoin(variable, "=", 0);
-	if (ft_strncmp(env, temp, ft_strlen(temp)) == 0 || ft_strncmp(env, variable, ft_strlen(variable) + 1) == 0)
+	if (ft_strncmp(env, temp, ft_strlen(temp)) == 0 || \
+		ft_strncmp(env, variable, ft_strlen(variable) + 1) == 0)
 		res = 0;
 	free(temp);
 	return (res);
@@ -31,12 +32,11 @@ char	**tab_join(char **tab, char *str)
 		i++;
 	}
 	new_tab[i] = ft_strdup(str);
-	// printf("new tab : %s\n", new_tab[i]);
 	new_tab[i + 1] = NULL;
 	return (new_tab);
 }
 
-char **tab_replace(char **env, char *args)
+char	**tab_replace(char **env, char *args)
 {
 	char	*variable;
 	int		j;
@@ -72,7 +72,7 @@ static int	check_arg(char *args)
 		return (1);
 	while (args[++i])
 		if (ft_isoperator(args[i]))
-			return(1);
+			return (1);
 	return (0);
 }
 
@@ -106,47 +106,65 @@ int	ft_isexport(char *args, char **env)
 	return (is_export);
 }
 
+int	ft_print_export(char **args, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	if (!args[1])
+	{
+		while (data->export[i++] && data->export[i] != NULL)
+			printf("%s\n", data->export[i]);
+		return (1);
+	}
+	return (0);
+}
+
+void ft_replace_export(char ***new_env, char ***new_export, int i, t_data *data)
+{
+	if (ft_isexport(data->cmds->args[i], data->my_env) && \
+				ft_isexport(data->cmds->args[i], data->export))
+	{
+		*new_env = tab_replace((data->my_env), data->cmds->args[i]);
+		*new_export = tab_replace((data->export), data->cmds->args[i]);
+	}
+	else if (!ft_isexport(data->cmds->args[i], data->my_env) && \
+			ft_isexport(data->cmds->args[i], data->export))
+	{
+		*new_env = tab_join((data->my_env), data->cmds->args[i]);
+		*new_export = tab_replace((data->export), data->cmds->args[i]);
+	}
+	else
+	{
+		*new_env = tab_join((data->my_env), data->cmds->args[i]);
+		*new_export = tab_join((data->export), data->cmds->args[i]);
+	}
+	if (!*new_env || !*new_export)
+		exit_minishell(&data->token, &data->cmds, data);
+}
+
 int	ft_export(char **args, t_data *data)
 {
 	int		i;
 	char	**new_env;
 	char	**new_export;
 
-	i = 0;
-	if (!args[1])
-	{
-		while(data->export[i++] && data->export[i] != NULL)
-			printf("%s\n", data->export[i]);
-		return(0);
-	}
-	i++;
+	new_env = NULL;
+	new_export = NULL;
+	i = 1;
+	if (ft_print_export(args, data) == 1)
+		return (0);
 	while (args[i])
 	{
 		if (check_arg(args[i]))
 		{
 			write(1, "not a valid identifier\n", 24);
 			i++;
-			continue;
+			continue ;
 		}
 		if (ft_strchr(args[i], '='))
 		{
-			if (ft_isexport(args[i], data->my_env) && ft_isexport(args[i], data->export))
-			{
-				new_env = tab_replace((data->my_env), args[i]);
-				new_export = tab_replace((data->export), args[i]);
-			}
-			else if (!ft_isexport(args[i], data->my_env) && ft_isexport(args[i], data->export))
-			{
-				new_env = tab_join((data->my_env), args[i]);
-				new_export = tab_replace((data->export), args[i]);
-			}
-			else
-			{
-				new_env = tab_join((data->my_env), args[i]);
-				new_export = tab_join((data->export), args[i]);
-			}
-			if (!new_env || !new_export)
-				return (-1);
+			ft_replace_export(&new_env, &new_export, i, data);
 			(data->my_env) = new_env;
 			(data->export) = new_export;
 		}
@@ -155,7 +173,7 @@ int	ft_export(char **args, t_data *data)
 			if (ft_isexport(args[i], data->export))
 			{
 				i++;
-				continue;
+				continue ;
 			}
 			new_export = tab_join((data->export), args[i]);
 			if (!new_export)
