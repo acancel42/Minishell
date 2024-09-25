9c7cd46be2fe6b4a81fe68b4a8ef7c1b95f17b5b
Adri-cncl <cancel.adrien@gmail.com>
2024-09-25 05:59:07 +0200

    norm akainu

--- src/token_init_utils.c
+++ src/token_init_utils.c
@@ -1,19 +1,9 @@
 #include "minishell.h"
 
-int	handle_output(char *src, t_token **token, t_data *data)
+static void	handle_output_util(t_token **token, t_data *data, int j)
 {
-	int		j;
-	int		i;
 	t_token	*temp;
 
-	i = 0;
-	(void)(data);
-	j = 0;
-	if (src && src[i + 1] == '>')
-	{
-		j++;
-		i++;
-	}
 	if (j == 0)
 	{
 		temp = ft_toknew('>', T_REDIR_OUT);
@@ -28,25 +18,30 @@ int	handle_output(char *src, t_token **token, t_data *data)
 			ft_exit(*token, NULL, data);
 		ft_tokadd_back(token, temp);
 	}
+}
+
+int	handle_output(char *src, t_token **token, t_data *data)
+{
+	int		j;
+	int		i;
+
+	i = 0;
+	j = 0;
+	if (src && src[i + 1] == '>')
+	{
+		j++;
+		i++;
+	}
+	handle_output_util(token, data, j);
 	data->rflag = 1;
 	i++;
 	return (i);
 }
 
-int	handle_input(char *src, t_token **token, t_data *data)
+static void	handle_input_util(t_token **token, t_data *data, int j)
 {
-	int		j;
-	int		i;
 	t_token	*temp;
 
-	(void)(data);
-	i = 0;
-	j = 0;
-	if (src && src[i + 1] == '<')
-	{
-		j++;
-		i++;
-	}
 	if (j == 0)
 	{
 		temp = ft_toknew('<', T_REDIR_IN);
@@ -61,6 +56,21 @@ int	handle_input(char *src, t_token **token, t_data *data)
 			ft_exit(*token, NULL, data);
 		ft_tokadd_back(token, temp);
 	}
+}
+
+int	handle_input(char *src, t_token **token, t_data *data)
+{
+	int		j;
+	int		i;
+
+	i = 0;
+	j = 0;
+	if (src && src[i + 1] == '<')
+	{
+		j++;
+		i++;
+	}
+	handle_input_util(token, data, j);
 	data->rflag = 1;
 	i++;
 	return (i);
@@ -78,6 +88,27 @@ int	handle_pipe(t_token **token, t_data *data)
 	return (1);
 }
 
+static t_token	*handle_else_util(char *src, t_token **token, \
+									t_data *data, int *i)
+{
+	t_token	*temp;
+
+	if (data->rflag == 0)
+	{
+		temp = ft_toknew(src[(*i)++], T_WORD);
+		if (!temp)
+			ft_exit(*token, NULL, data);
+	}
+	else
+	{
+		data->rflag = 0;
+		temp = ft_toknew(src[(*i)++], T_RWORD);
+		if (!temp)
+			ft_exit(*token, NULL, data);
+	}
+	return (temp);
+}
+
 int	handle_else(char *src, t_token **token, t_data *data)
 {
 	char	c;
@@ -85,20 +116,7 @@ int	handle_else(char *src, t_token **token, t_data *data)
 	t_token	*temp;
 
 	i = 0;
-	(void)(data);
-	if (data->rflag == 0)
-	{
-		temp = ft_toknew(src[i++], T_WORD);
-		if (!temp)
-			ft_exit(*token, NULL, data);
-	}
-	else
-	{
-		data->rflag = 0;
-		temp = ft_toknew(src[i++], T_RWORD);
-		if (!temp)
-			ft_exit(*token, NULL, data);
-	}
+	temp = handle_else_util(src, token, data, &i);
 	ft_tokadd_back(token, temp);
 	while (src[i] && !ft_iswspace(src[i]) && !ft_isoperator(src[i]) && \
 		!ft_isquote(src[i]))
